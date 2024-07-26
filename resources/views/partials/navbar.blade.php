e6f3251a431cbea8aa293052cf3aa4186ad665ce
IT21248030 <99585390+IT21248030@users.noreply.github.com>
2024-07-26 23:12:35 +0530

    Add loging and register routes

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -169,7 +169,7 @@
 
                     <!-- Navbar Login-->
                     <li class="ms-1 d-none d-lg-inline-block">
-                        <a class="btn btn-link px-2 text-decoration-none d-flex align-items-center" href="#">
+                        <a class="btn btn-link px-2 text-decoration-none d-flex align-items-center"href="{{ route('login') }}">
                             <i class="ri-user-line ri-lg align-middle"></i>
                         </a>
                     </li>
@@ -179,7 +179,7 @@
                     <li class="ms-1 d-inline-block position-relative">
                         <button class="btn btn-link px-2 text-decoration-none d-flex align-items-center disable-child-pointer" data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
                             <i class="ri-shopping-cart-2-line ri-lg align-middle position-relative z-index-10"></i>
-                            <span class="fs-xs fw-bolder f-w-5 f-h-5 bg-orange rounded-lg d-block lh-1 pt-1 position-absolute top-0 end-0 z-index-20 mt-2 text-white">2</span>
+                            <!-- <span class="fs-xs fw-bolder f-w-5 f-h-5 bg-orange rounded-lg d-block lh-1 pt-1 position-absolute top-0 end-0 z-index-20 mt-2 text-white">1</span> -->
                         </button>
                     </li>
                     <!-- /Navbar Cart-->
